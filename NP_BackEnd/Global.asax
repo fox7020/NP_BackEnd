4932e222a8016b58608b29827d134058221eccbe
Wayne Lai <cdjh32@gmail.com>
2017-06-05 13:45:42 +0800

    Create Project & Connect DB

---
+++ NP_BackEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NP_BackEnd.MvcApplication" Language="C#" %>
---
+++ NP_BackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NP_BackEnd.Startup))]
+namespace NP_BackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
